0baafa2b75996b44b54f94ba3e35dc2c124eaec8
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-08 13:46:03 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print out numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	putchar('\n');
+	return (0);
+}
