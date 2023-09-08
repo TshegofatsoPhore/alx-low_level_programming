89af910acf57b9738702f72469ebb5f933a24b42
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-08 14:49:27 -0700

    Task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print out numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(48 + digit);
+		if (digit < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
