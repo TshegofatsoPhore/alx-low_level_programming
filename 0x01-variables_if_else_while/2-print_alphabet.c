b597d11a4bc4b818e1ecd8be09b64ed017944f42
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-08 12:36:24 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print out alphabets in lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowerChar = 'a';
+
+	for (lowerChar = 'a'; lowerChar <= 'z'; lowerChar++)
+	{
+		putchar(lowerChar);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print out alphabets in lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowerChar = 'a';
+	char upperChar = 'A';
+
+	for (lowerChar = 'a'; lowerChar <= 'z'; lowerChar++)
+	{
+		putchar(lowerChar);
+	}
+	for (upperChar = 'A'; upperChar <= 'Z'; upperChar++)
+	{
+		putchar(upperChar);
+	}
+	return (0);
+}
