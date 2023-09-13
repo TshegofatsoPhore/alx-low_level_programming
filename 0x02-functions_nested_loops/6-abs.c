10961c12895a1de2cb83b0938e683a7d8f77e33f
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-13 13:28:08 -0700

    Task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of integer
+ *@a: function parameter
+ * Return: a or -a
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else if (a >= 0)
+		return (a);
+	return (0);
+}
