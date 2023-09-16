32550ee81546b02b3c123b72a273cdaddc45e506
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-15 18:45:00 -0700

    Task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - function prints square the #
+ * @size: parameter size of square
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int a, b;
+
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
