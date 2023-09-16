8583e3eb4773664a7b637f88b1cb20502c020b6b
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-15 17:28:18 -0700

    Task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,9 @@ void more_numbers(void)
 	{
 		for (b = 0; b < 15; b++)
 		{
-			if (b < 10)
-				_putchar(b + '0');
-			else if (b >= 10)
-			{
+			if (b > 9)
 				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
-			}
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
