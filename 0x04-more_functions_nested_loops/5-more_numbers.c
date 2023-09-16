4c634ada3699c97aeb55d459a5a4fc318b7d8376
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-15 17:06:28 -0700

    Task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints set of numbers 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b < 10)
+				_putchar(b + '0');
+			else if (b >= 10)
+			{
+				_putchar(b / 10 + '0');
+				_putchar(b % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
