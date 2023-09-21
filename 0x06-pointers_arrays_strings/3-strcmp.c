6e257daf28273cfdcbd2ed1140ba223e08aae38e
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-21 14:46:15 -0700

    Task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - function compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			if (*s1 < *s2)
+				return (*s1 - *s2);
+			else if (*s2 < *s1)
+				return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
+}
