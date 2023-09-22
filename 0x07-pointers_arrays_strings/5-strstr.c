dc82f9f5fe3615bc7f449670a98b14d3afba8892
TshegofatsoPhore <kamogelophore@gmail.com>
2023-09-22 16:43:42 -0700

    Task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: contains string
+ * @needle: has the substring
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		a = 0;
+		if (haystack[a] == needle[a])
+		{
+			do {
+				if (needle[a + 1] == '\0')
+					return (haystack);
+				a++;
+
+			} while (haystack[a] == needle[a]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
