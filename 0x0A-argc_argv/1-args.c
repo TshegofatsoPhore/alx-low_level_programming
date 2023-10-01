633cc7520e77e11bf774e0c52ca5f1894d1de942
TshegofatsoPhore <kamogelophore@gmail.com>
2023-10-01 03:45:04 -0700

    Task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed
+ *@argc: arguments count
+ *@argv: arguments veector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
